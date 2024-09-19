f1e43fc42dd994a3fc8da36b4545bdccf65f4ebc
KAMINI <146583090+kamini-pal@users.noreply.github.com>
2024-09-19 23:18:47 +0530

    day2

---
+++ day1/day2/index.js
@@ -0,0 +1,9 @@
+let element = document.getElementById('fpara');
+element.style.backgroundColor = "black";
+element.style.color = "white";
+
+let element2 = document.getElementById('spara');
+element2.style.cssText = "background-color:green; color:white";
+
+let element3 = document.getElementById('spara');
+element3.setAttribute('style', 'font-weight:bold');
